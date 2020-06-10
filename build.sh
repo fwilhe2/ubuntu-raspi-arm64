fdf8c5d7dae6b6da8727e883f5871d99ece22837
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2020-06-10 22:05:23 +0200

    Group output in gh actions

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-
+echo '::group::Setup Environment'
 if [ `id -u` -ne 0 ];then
     echo "Please run this script with 'sudo' or by root user."
     exit 1
@@ -9,20 +9,31 @@ whereisme=$whereisme $whereisme/scripts/environment.sh
 if [ $? -ne 0 ]; then
     exit 1
 fi
+echo '::endgroup'
+echo '::group::Create Image'
 whereisme=$whereisme $whereisme/scripts/image.sh
 if [ $? -ne 0 ]; then
     exit 1
 fi
+echo '::endgroup'
+echo '::group::Building boot partition'
 whereisme=$whereisme $whereisme/scripts/boot.sh
 if [ $? -ne 0 ]; then
     exit 1
 fi
+echo '::endgroup'
+echo '::group::Building root file system'
 whereisme=$whereisme $whereisme/scripts/rootfs.sh
 if [ $? -ne 0 ]; then
     exit 1
 fi
+echo '::endgroup'
+echo '::group::Building linux kernel'
 whereisme=$whereisme $whereisme/scripts/kernel.sh
 if [ $? -ne 0 ]; then
     exit 1
 fi
+echo '::endgroup'
+echo '::group::Cleaning'
 whereisme=$whereisme $whereisme/scripts/final.sh
+echo '::endgroup'
